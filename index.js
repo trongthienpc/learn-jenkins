b9cbeb28bf8af116671b20c16ef7ae9b43dda84e
thientt <thientt.bdst@gmail.com>
2023-06-18 05:24:48 +0700

    add-api-test-request

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
+app.get("/api/", (req, res) => {
+  res.send("This is a test request");
+});
+
 app.listen(3000, (err, res) => {
   console.log("Server is listening on port 3000");
 });
